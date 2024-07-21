e04b3d8daa9479d62518b23ae434ab4e8aead766
Zachary Saile <82775910+zwsaile@users.noreply.github.com>
2024-07-20 19:28:58 -0600

    Delete eleven-mile-retreat directory

--- eleven-mile-retreat/src/app/navbar.js
+++
@@ -1,8 +0,0 @@
-export default function Navbar() {
-    return <nav className="flex justify-between bg-black border-none p-3">
-        <div className="left-nav flex justify-center items-center">
-            <img src="/arrowhead.jpeg" alt="Site logo" className="w-[10vw] h-auto" />
-        </div>
-    </nav>
-  }
-  
